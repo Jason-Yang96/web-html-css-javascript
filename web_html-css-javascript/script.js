4489600b94622103372f725b0ea61a5b716108b4
Jason Yang <didwotjs120@gmail.com>
2023-10-23 10:53:52 +0900

    s

--- web_html-css-javascript/script.js
+++ web_html-css-javascript/script.js
@@ -2,3 +2,14 @@ console.log('안녕하세요');
 console.log(123);
 console.log(true);
 var greeting = "안녕하세요";
+console.log({a: "a", b: "b"})
+console.table({a: "a", b: "b"})
+
+console.error("eroo!");
+console.warn("warning");
+
+console.time("hello");
+console.log(1);
+console.log(2);
+console.timeEnd("hello");
+
